c6fe0eeff5806dc17360ca485ee2d55cfab4656e
VerselT <Verseltristan@gmail.com>
2024-12-16 15:08:11 +0100

    phishing done

--- app/src/services/gophishService.ts
+++ app/src/services/gophishService.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+console.log('API_URL:', API_URL);
+console.log('API_KEY:', API_KEY);
 
 const gophishService = {
 
